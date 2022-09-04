e7ae094b7e7cd4bb0f6cdb76d2503d66d4faa361
Shane Horton <shanehortonm@gmail.com>
2022-09-04 08:03:00 -0400

    work on gallery

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector("header");
     header.classList.toggle("", window.scrollY > 0);
 })
+
+
+
+
+// Start of Gallery
+const boxes = document.querySelectorAll(".box");
+
+checkBoxes();
+window.addEventListener("scroll", checkBoxes);
+
+function checkBoxes() {
+	const triggerBottom = (window.innerHeight / 5) * 2.8;
+	
+	boxes.forEach((box) => {
+		let boxTop = box.getBoundingClientRect().top;
+		boxTop < triggerBottom
+			? box.classList.add("show")
+			: box.classList.remove("show");
+	});
+}
