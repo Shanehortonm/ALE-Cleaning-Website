5a97a517b33c026f3a5b4d5f90f28cb273c79889
Shanehortonm <shanehortonm@gmail.com>
2022-12-07 07:48:29 -0500

    changes to nav and index

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector("header");
-    header.classList.toggle("", window.scrollY > 0);
+//Start of Hamburger
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener ("click", () => {
+	hamburger.classList.toggle("active");
+  	navMenu.classList.toggle("active");
 })
 
-
-
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  	hamburger.classList.remove("active");
+  	navMenu.classList.remove("active");
+})) 
 
 // Start of Gallery
 const boxes = document.querySelectorAll(".box");
