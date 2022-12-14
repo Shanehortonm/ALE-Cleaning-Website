a66b5f78e1a292889f80e691775e8926e1fbcd21
Shane Horton <shanehortonm@gmail.com>
2022-12-14 18:00:45 -0500

    navbar work

--- script.js
+++ script.js
@@ -18,4 +18,15 @@ function checkBoxes() {
 
 // Start of Contact Form
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+  });
+
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+  }));
